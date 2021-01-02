b7a5245695548ecbb8320c70ef92e7e4daaff27a
takeike <takemasa.ikeda@konicaminolta.com>
2021-01-02 18:48:19 +0900

    learn compornet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
 
-class App extends Component{
-  render(){
-    return(
+// class App extends Component{
+//   render(){
+//     return(
 
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() =>{console.log("I am clicked")}} />
-    </React.Fragment>
-    )
-    // return <input type="text" onChange={() =>{console.log("I am clicked")}} />;
-    // const greeting = "Hi, Tom"
-    // const dom = <h1 className="foo">{greeting}</h1>
-    // return dom;
-    // return <h1>Hello, world</h1>
-  }
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() =>{console.log("I am clicked")}} />
+//     </React.Fragment>
+//     )
+//     // return <input type="text" onChange={() =>{console.log("I am clicked")}} />;
+//     // const greeting = "Hi, Tom"
+//     // const dom = <h1 className="foo">{greeting}</h1>
+//     // return dom;
+//     // return <h1>Hello, world</h1>
+//   }
+// }
+
+const App = () => {
+  return<div>Hi!</div>
 }
 
 export default App;
