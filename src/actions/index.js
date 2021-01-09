9389a740704ad374a9e789508449690ec786db19
takeike <takemasa.ikeda@konicaminolta.com>
2021-01-09 19:23:05 +0900

    implement show

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const READ_EVENT = 'READ_EVENT'
 export const CREATE_EVENTS = 'CREATE_EVENTS'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -16,6 +18,18 @@ export const postEvent = values => async dispatch => {
     const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
     dispatch({type: CREATE_EVENTS, response})
 }
+
+export const putEvent = values => async dispatch => {
+    const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+    dispatch({type: UPDATE_EVENT, response})
+}
+
+export const getEvent = id => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    console.log(response)
+    dispatch({type: READ_EVENT, response})
+}
+
 
 
 export const deleteEvent = id => async dispatch => {
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
-import {READ_EVENTS, DELETE_EVENT} from '../actions'
+import {CREATE_EVENTS, READ_EVENT,READ_EVENTS, DELETE_EVENT, UPDATE_EVENT} from '../actions'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (events={}, action) => {
     switch(action.type){
+        case CREATE_EVENTS:
+        case UPDATE_EVENT:
+        case READ_EVENT:
+            const data =action.response.data
+            return {...events, [data.id]: data}
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
         case DELETE_EVENT:
